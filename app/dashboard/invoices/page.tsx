9b3eb045ea9dd265a8d47b2f9fdf9d8903ef9682
Morris Oliha <niyemayo@gmail.com>
2026-05-19 23:50:18 +0100

    Fix pagination currentPage prop

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -23,6 +23,7 @@ export default async function Page(props: {
   const searchParams = await props.searchParams;
 
   const query = searchParams?.query || '';
+
   const currentPage = Number(searchParams?.page) || 1;
 
   const totalPages = await fetchInvoicesPages(query);
@@ -45,11 +46,17 @@ export default async function Page(props: {
         key={query + currentPage}
         fallback={<InvoicesTableSkeleton />}
       >
-        <Table query={query} currentPage={currentPage} />
+        <Table
+          query={query}
+          currentPage={currentPage}
+        />
       </Suspense>
 
       <div className="mt-5 flex w-full justify-center">
-        <Pagination totalPages={totalPages} />
+        <Pagination
+          totalPages={totalPages}
+          currentPage={currentPage}
+        />
       </div>
     </div>
   );
