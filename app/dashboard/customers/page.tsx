811ae6c5bb50465fde8cff91419302bef22b6a90
Morris Oliha <niyemayo@gmail.com>
2026-05-20 18:23:19 +0100

    Pass customers data to table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,20 @@
-
 import type { Metadata } from 'next';
+
+import CustomersTable from '@/app/ui/customers/table';
+
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page() {
+  const customers =
+    await fetchFilteredCustomers('');
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 }
