047cb992658f525d357f48744072d2ebed7b7272
Morris Oliha <niyemayo@gmail.com>
2026-05-20 11:15:59 +0100

    Fix the rest imports and proxy

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,7 +2,7 @@ import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { CreateInvoice } from '@/app/ui/invoices/button';
 
 import { lusitana } from '@/app/ui/fonts';
 
